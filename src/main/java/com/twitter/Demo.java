eb9b892f8f5ad6816c165b334d8bc79df1340cdc
sundar <sundar@gmail.com>
2022-12-03 23:41:51 +0530

    fully updted

---
+++ src/main/java/com/twitter/Demo.java
@@ -0,0 +1,9 @@
+package com.twitter;
+public class Demo{
+
+	public static void main(String ar[]){
+
+		System.out.println("this is sundar demo program");
+		System.out.println("one more update");
+	}
+}
